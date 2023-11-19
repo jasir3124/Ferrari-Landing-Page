1b83b9cf72a094a10084b6e6ae15802a2995f0e6
Jasir Limani <jasirlimani12@gmail.com>
2023-11-19 14:41:48 +0100

    made progress

--- main.js
+++ main.js
@@ -12,3 +12,31 @@ sideBarBtnClose.addEventListener("click", function () {
   sideBar.style.display = "none";
   body.style.overflow = "visible";
 });
+
+
+
+
+let ferrariImage = document.querySelectorAll('.ferrariImageSection')
+
+ferrariImage.forEach(image => {
+  image.addEventListener('mouseover', function () {
+    zoomIn(image);
+  })
+  image.addEventListener('mouseout', function () {
+    zoomOut(image);
+  })
+});
+function zoomIn(image){
+  anime({
+    targets: image,
+    scale : 1.1,
+    easing: 'easeInOutQuad',
+  })
+}
+function zoomOut(image) {
+  anime({
+    targets: image,
+    scale : 1,
+    easing: 'easeInOutQuad',
+  })
+}
